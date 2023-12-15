82a2bb22c9bc08511ef0b02c22390b1c2634e6bc
HelenaSusan <c14220149@john.petra.ac.id>
2023-12-15 21:41:48 +0700

    admin input file

--- write_file.cpp
+++ write_file.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include "Graph.h"
+#include "customer.h"
+#include "admin.h"
 using namespace std;
 
 int main(){
-    // test edit file di vs code
+    Admin A[2];
+    A[0].add("Helena", "helena@gmail.com", "123", "helenapw");
+    A[0].add("Aiko", "aiko@gmail.com", "123", "aikopw");
+    fstream f1;
+    f1.open("admin.txt", ios::out);
+    for (int i = 0; i <= 1; i++) {
+        f1.write((char*)&A[i], sizeof(A[i]));
+    }
+    f1.close();
 }
